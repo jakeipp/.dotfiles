affceab141b9fc21527acbb70759c910000e70d0
Jake Ippolito <jake@pop-os.localdomain>
2022-10-28 13:24:37 -0400

    Update install

--- install.sh
+++ install.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 echo "Be sure to install package: fzf"
-echo "Be sure to install package: npm"
-echo "Be sure to install package: yarn (npm install --global yarn)"
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# echo "COC Dependancies:"
+# echo "\tBe sure to install package: npm"
+# echo "\tBe sure to install package: yarn (npm install --global yarn)"
+
+# Install Vundle (Vim)
+# git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Install Tmux Plugin Manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+     
+# Install Packer (NVim)
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
 cp .vimrc ~/.vimrc
 cp .tmux.conf ~/.tmux.conf
-vim +PluginInstall
+# vim +PluginInstall
+nvim +PackerSync
 echo "Make sure to install tmux plugins with <prefix> + I"
